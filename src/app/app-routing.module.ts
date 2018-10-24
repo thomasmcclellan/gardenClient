d33576df5f16f36279d95b48ca694f50b760a4c1
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-10-24 11:42:40 -0400

    Added auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from './products/products.component';
 import {AboutComponent} from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes:Routes=[
   {path:'products', component:ProductsComponent},
   {path:'about', component:AboutComponent},
   {path:'home', component:HomeComponent},
+  {path:'auth', component:AuthComponent},
   {path: '' , redirectTo:'/home',pathMatch:'full'},
   {path:'*', redirectTo:'/home', pathMatch:'full'}
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     AboutComponent,
     ProductsComponent,
-    HomeComponent
+    HomeComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
